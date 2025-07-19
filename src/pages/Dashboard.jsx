1b6d93cea975bdb058ea8714630c9b7ef447ba36
Jacob Casas <95025463+jacobcasas@users.noreply.github.com>
2025-07-19 11:22:39 -0700

    setting up react jest to test components before putting them to DOM

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,15 @@
+import '../styles/pages/dashboard.css';
+
 function Dashboard () {
-    
+    const user = "jake";
     return (
         <>
             <div className="page-container">
-                <h1>Hello</h1>
+                <div className="hero-section">
+                    <p className="indexed | bold"><i>Indexed</i></p>
+                    <h2 className='color-gray-400'>welcome back, <span className='username | color-gray-50'>{user}</span></h2>
+                    <div className="week-tracker-container"></div>
+                </div>
             </div>
         </>
     )
