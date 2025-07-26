8fc8973eae8a92788721bde243ba98143071d0fd
Jacob Casas <95025463+jacobcasas@users.noreply.github.com>
2025-07-25 18:40:57 -0700

    created a localStorage object for storing the data the user enters for a new deck

--- src/pages/DeckCreation.jsx
+++ src/pages/DeckCreation.jsx
@@ -1,27 +1,52 @@
 import '../styles/pages/deckcreation.css';
 import Button from '../components/Button/Button';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function DeckCreation () {
     const [visibility, setVisibility] = useState(false);
     const [isDisabled, setIsDisabled] = useState(true);
     const [categoryInput, setCategoryInput] = useState('');
-    const [user, setUser] = useState(() => {
-        const stored = localStorage.getItem("user");
-        return stored ? JSON.parse(stored) : {name: "Jacob", age: 26, hobbies: []}
+    const [titleInput,setTitleInput] = useState('');
+    const [descriptionInput, setDescriptionInput] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const [categories, setCategories] = useState(() => {
+        const storedCategories = localStorage.getItem("categories");
+        return storedCategories 
+            ? JSON.parse(storedCategories) 
+            : {
+                categoryList: []
+            }
     });
 
-    useEffect(() => {
-        localStorage.setItem("user", JSON.stringify(user))
-    }, [user]);
+    const [deckDetails, setDeckDetails] = useState(() => {
+        const storedDetails = localStorage.getItem("details");
+        return storedDetails
+            ? JSON.parse(storedDetails)
+            : {
+                category: "okie",
+                deckTitle: "nice",
+                deckDescription: "okeeee"
+            }
+    })
 
     useEffect(() => {
-        console.log(user.hobbies);
-    }, [user])
+        localStorage.setItem("categories", JSON.stringify(categories));
+        localStorage.setItem("details", JSON.stringify(deckDetails))
+    }, [categories, deckDetails]);
+
+    useEffect(() => {
+        console.log(categories, deckDetails);
+    }, [categories, deckDetails])
     
-    const createCategoryBtnTest = e => {
+    const showCategoryCreator = e => {
         e.preventDefault();
         setVisibility(!visibility);
+    }
+
+    const selectCategory = (e) => {
+        e.preventDefault();
+        setSelectedCategory();
     }
 
     const toggleDisabled = (e) => {
@@ -30,27 +55,51 @@ function DeckCreation () {
         setIsDisabled(value.trim().length === 0);
     };
 
-    const appendNewCategory = (e) => {
+    const createNewCategory = (e) => {
         e.preventDefault();
         if (categoryInput.trim() === '') return;
 
-        setUser(prev => ({
+        setCategories(prev => ({
             ...prev,
-            hobbies: [...prev.hobbies, categoryInput.trim()]
+            categoryList: [...prev.categoryList, categoryInput.trim()]
         }));
 
         setCategoryInput('');
         setIsDisabled(true);
     };
 
+    const createNewDeck = (e) => {
+        e.preventDefault();
+        if (titleInput.trim() === '' && descriptionInput.trim() === '') return;
+
+        setDeckDetails(() => ({
+            category: {selectedCategory},
+            deckTitle: {titleInput},
+            deckDescription: {descriptionInput}
+        }));
+    };
+
+
+    const removeCategoriesTest = () => {
+        localStorage.removeItem("categories");
+    }
+
     return (
         <>
             <div className="page-container">
                 <h1 className='color-gray-50'>Create A Deck</h1>
-                <form action="" className='create-deck-form'>
+                <form className='create-deck-form'>
                     <div className="form-element">
                         <label htmlFor="category">Category</label>
-                        <Button label="+" type="add" onclick={(e) => createCategoryBtnTest(e)}/>
+                        <div className="category-buttons-container">
+                            {categories.categoryList.map(el => {
+                                return <Button label={el} type={el === selectedCategory ? `attention` : `neutral`} value={el} onclick={e => {
+                                    e.preventDefault();
+                                    setSelectedCategory(el);
+                                }}/>
+                            })}
+                            <Button label="+" type="add" onclick={(e) => showCategoryCreator(e)}/>
+                        </div>
                         <div className={`create-category ${!visibility ? 'hidden' : ''}`}>
                             <label htmlFor="category-list">Create a new category</label>
                             <div className="cc-input-and-button">
@@ -64,7 +113,7 @@ function DeckCreation () {
                                 <Button
                                  label="➔" 
                                  type="append" 
-                                 onclick={appendNewCategory} 
+                                 onclick={createNewCategory} 
                                  disabled={isDisabled}
                                 />
                             </div>
@@ -77,6 +126,8 @@ function DeckCreation () {
                          name="title" 
                          id="deck-title" 
                          placeholder='What did you want to study? i.e. "Spanish"'
+                         value={titleInput}
+                         onChange={e => setTitleInput(e.target.value)}
                           />
                     </div>
                     <div className="form-element">
@@ -84,8 +135,24 @@ function DeckCreation () {
                         <textarea
                          name="desciption" 
                          id="deck-description"
-                         placeholder='Why are you making this deck? i.e. "For the test Friday"'></textarea>
+                         placeholder='Why are you making this deck? i.e. "For the test Friday"'
+                         rows= "5"
+                         value={descriptionInput} 
+                         onChange={e => setDescriptionInput(e.target.value)}>
+                        </textarea>
                     </div>
+
+                    <Button 
+                        label="Create Deck"
+                        type="confirm"
+                        onclick={createNewDeck}
+                    />
+
+                    <Link to="/">
+                        <Button label="Go Home" type="neutral" />
+                    </Link>
+                    
+                     
                 </form>
             </div>
         </>
