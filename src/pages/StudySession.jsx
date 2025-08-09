d5faf3ed95f35ff7bd96086adf5f95a50162d360
Jacob Casas <95025463+jacobcasas@users.noreply.github.com>
2025-08-09 02:16:53 -0700

    EditDeck functions added, not sure if finished. edited some styles around the app.

--- src/pages/StudySession.jsx
+++ src/pages/StudySession.jsx
@@ -10,7 +10,32 @@ function StudySession () {
     const { deck, setDeck, currentCardIndex, setCurrentCardIndex } = useDeck(deckId);
     const [showBack, setShowBack] = useState(false);
     const [correctCount, setCorrectCount] = useState(0);
+    const [incorrectCount, setincorrectCount] = useState([]);
     const [isSessionComplete, setIsSessionComplete] = useState(false);
+
+    useEffect(() => {
+        setCurrentCardIndex(0);
+    }, [deck]);
+
+    const handleCorrectAnswer = () => {
+        setCorrectCount(correctCount + 1);
+        goToNextCard();
+    }
+
+    const handleIncorrectAnswer = () => {
+        setincorrectCount(prev => [...prev, deck.cards[currentCardIndex]]);
+        goToNextCard();
+    }
+
+    const goToNextCard = () => {
+        const nextIndex = currentCardIndex + 1;
+        if (currentCardIndex === deck.cards.length) {
+            setIsSessionComplete(true);
+        } else {
+            setCurrentCardIndex(nextIndex);
+            setShowBack(false);
+        }
+    }
 
     if (!deck) return <p>Deck not found...</p>
     
@@ -20,8 +45,19 @@ function StudySession () {
                 <h1>Study Session: {deck.title}</h1>
                 <h5 className="color-gray-400">{deck.description}</h5>
             </header>
+            <h3 className="center-text">Card {currentCardIndex}/{deck.cards.length}</h3>
             <div className="study-card">
-                
+                <h5>
+                    {showBack 
+                        ? deck.cards[currentCardIndex]?.back
+                        : deck.cards[currentCardIndex]?.front
+                    }
+                </h5>
+            </div>
+            <div className="under-card-buttons">
+                <Button label="X" type="attention"/>
+                <Button label="Flip Card" type="neutral"/>
+                <Button label="&#x2713;" type="success"/>
             </div>
         </div>
     );
