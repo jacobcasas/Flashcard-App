f18a9c798b9fe40f47c31ce19bccc0c9e7e7ac92
Jacob Casas <jacobcasas17@gmail.com>
2025-08-09 06:12:30 -0700

    finished study session. next i'll build the completed session screen.

--- src/pages/StudySession.jsx
+++ src/pages/StudySession.jsx
@@ -29,7 +29,7 @@ function StudySession () {
 
     const goToNextCard = () => {
         const nextIndex = currentCardIndex + 1;
-        if (currentCardIndex === deck.cards.length) {
+        if (currentCardIndex === deck.cards.length - 1) {
             setIsSessionComplete(true);
         } else {
             setCurrentCardIndex(nextIndex);
@@ -38,6 +38,12 @@ function StudySession () {
     }
 
     if (!deck) return <p>Deck not found...</p>
+
+    if (isSessionComplete) {
+        return (
+            <h1>Hello World!</h1>
+        )
+    }
     
     return (
         <div className="page-container">
@@ -45,19 +51,30 @@ function StudySession () {
                 <h1>Study Session: {deck.title}</h1>
                 <h5 className="color-gray-400">{deck.description}</h5>
             </header>
-            <h3 className="center-text">Card {currentCardIndex}/{deck.cards.length}</h3>
-            <div className="study-card">
-                <h5>
-                    {showBack 
-                        ? deck.cards[currentCardIndex]?.back
-                        : deck.cards[currentCardIndex]?.front
-                    }
-                </h5>
+
+            <div className="study-card-and-count">
+                <h3 className="center-text">Card {currentCardIndex + 1}/{deck.cards.length}</h3>
+                <div className="study-card">
+                    <h4>
+                        {showBack 
+                            ? deck.cards[currentCardIndex]?.back
+                            : deck.cards[currentCardIndex]?.front
+                        }
+                    </h4>
+                </div>
             </div>
-            <div className="under-card-buttons">
-                <Button label="X" type="attention"/>
-                <Button label="Flip Card" type="neutral"/>
-                <Button label="&#x2713;" type="success"/>
+            
+            <div className="study-under-card">
+                <Button label="Flip Card" type="neutral" onclick={() => setShowBack(!showBack)}/>
+                <div className="correct-prompt">
+                    <h5>Did you guess correctly?</h5>
+                    <div className="prompt-buttons">
+                        <Button label="X" type="attention" onclick={() => handleCorrectAnswer()}/>
+                        <Button label="&#x2713;" type="success" onclick={() => handleIncorrectAnswer()}/>
+                    </div>
+                    
+                </div>
+                
             </div>
         </div>
     );
